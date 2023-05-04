b661c5293f6246dba67650c43b8af4a2614a7c2a
ChaoqunSu <1728448550@qq.com>
2023-05-04 22:05:29 +0800

    first commit

--- 基础推荐算法/UserCF.py
+++ 基础推荐算法/UserCF.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import numpy as np
+import warnings
+import random, math, os
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+warnings.filterwarnings('ignore')
